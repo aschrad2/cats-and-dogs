23f86cae05048b21e0cdd8754dbf99e3cca96cb1
Student <example@example.com>
2020-01-23 19:52:22 -0800

    Add jquery to add images to site on button click.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
-    $("button#hello").click(function() {
-        $("ul#user").prepend("<li>Hello!</li>");
-        $("ul#webpage").prepend("<li>Why hello there!</li>");
-        // $('li').css('background-color', 'green');
-        // $("li").click(function() {
-        //     alert("hi");
-        // });
-        $("ul#user").children("li").first().click(function() {
-            $(this).remove();
-        });
-        $("ul#webpage").children("li").first().click(function() {
-            $(this).remove();
-        });
+    let catCounter = 0;
+    let dogCounter = 0;
+
+
+    $('button#catOne').click(function(){
+        urlLink = "";
+        if (catCounter == 0) {
+            urlLink = "https://cdn.pixabay.com/photo/2014/04/29/13/19/cat-334383_1280.jpg"      
+        } else if (catCounter == 1) {
+            urlLink = "https://cdn.pixabay.com/photo/2017/11/14/13/06/kitty-2948404_1280.jpg"
+        } else if (catCounter == 2) {
+            urlLink="https://cdn.pixabay.com/photo/2016/01/19/17/41/friends-1149841_1280.jpg"
+        }
+        $('#catImage').append(`<img src='${urlLink}'>`);
+        catCounter = catCounter + 1;
+        console.log(catCounter);
     });
-    $("button#goodbye").click(function() {
-        $("ul#user").prepend("<li>Goodbye!</li>")
-        $("ul#webpage").prepend("<li>Good bye, dear user!</li>");
-        $("ul#user").children("li").first().click(function() {
-            $(this).remove();
-        });
-        $("ul#webpage").children('li').first().click(function() {
-            $(this).remove();
-        });
+
+    $("button#dogOne").click(function() {
+        urlLink = "";
+        if (dogCounter == 0) {
+            urlLink="https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg"
+        } else if (dogCounter == 1) {
+            urlLink="https://cdn.pixabay.com/photo/2014/08/21/14/51/pet-423398_1280.jpg"
+        } else if (dogCounter === 2) {
+            urlLink="https://cdn.pixabay.com/photo/2017/04/03/17/35/animals-2198994_1280.jpg"
+        }
+        $("#dogImage").append(`<img src='${urlLink}'>`);
+        dogCounter += 1;
+
     });
-    $("button#stop").click(function() {
-        $("ul#user").prepend("<li>Stop copying me!</li>");
-        $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
-        $("ul#user").children('li').first().click(function() {
-            $(this).remove();
-        })
-        $("ul#webpage").children('li').first().click(function() {
-            $(this).remove();
-        });
-    });
+
+
+
 });
